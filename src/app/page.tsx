ef7e19e178e1db31bbb7c8ccce9967ab6454b53b
agrotlaloc <agrotlaloc@gmail.com>
2023-08-21 21:50:10 -0600

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,13 @@ import { useState } from 'react'
 import { Dialog } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 
+
 const navigation = [
-  { name: 'Product', href: '#' },
-  { name: 'Features', href: '#' },
-  { name: 'Marketplace', href: '#' },
-  { name: 'Company', href: '#' },
+  { name: 'Inicio', href: '#' },
+  { name: 'Marcas', href: '#' },
+  { name: 'Proyectos', href: '#' },
+  { name: 'Empresa', href: '#' },
+  { name: 'Tienda', href: 'https://shop.dbit.com.mx/' },
 ]
 
 export default function Example() {
@@ -16,14 +18,15 @@ export default function Example() {
 
   return (
     <div className="bg-white">
+      
       <header className="absolute inset-x-0 top-0 z-50">
         <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
           <div className="flex lg:flex-1">
             <a href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">Your Company</span>
+              <span className="sr-only">Dbit</span>
               <img
                 className="h-8 w-auto"
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                src="logo.png"
                 alt=""
               />
             </a>
@@ -56,10 +59,10 @@ export default function Example() {
           <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
             <div className="flex items-center justify-between">
               <a href="#" className="-m-1.5 p-1.5">
-                <span className="sr-only">Your Company</span>
+                <span className="sr-only">Dbit Inc</span>
                 <img
                   className="h-8 w-auto"
-                  src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                  src="logo.png"
                   alt=""
                 />
               </a>
@@ -99,7 +102,10 @@ export default function Example() {
         </Dialog>
       </header>
 
+      
+
       <div className="relative isolate px-6 pt-14 lg:px-8">
+        
         <div
           className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
           aria-hidden="true"
@@ -112,33 +118,35 @@ export default function Example() {
             }}
           />
         </div>
+
+        
+
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
           <div className="hidden sm:mb-8 sm:flex sm:justify-center">
             <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-              Announcing our next round of funding.{' '}
+            Anunciamos nuestra próxima ronda de financiación.{' '}
               <a href="#" className="font-semibold text-indigo-600">
                 <span className="absolute inset-0" aria-hidden="true" />
-                Read more <span aria-hidden="true">&rarr;</span>
+                leer más <span aria-hidden="true">&rarr;</span>
               </a>
             </div>
           </div>
           <div className="text-center">
             <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-              Data to enrich your online business
+              Muy pronto, conoce nuestra plataforma.
             </h1>
             <p className="mt-6 text-lg leading-8 text-gray-600">
-              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
-              fugiat veniam occaecat fugiat aliqua.
+            Dbit Inc. lidera la innovación tecnológica en e-commerce, medios, finanzas y movilidad. Impulsamos el futuro con soluciones disruptivas en conectividad y más, redefiniendo la experiencia en múltiples industrias.
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <a
                 href="#"
                 className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
-                Get started
+                Comenzar
               </a>
               <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-                Learn more <span aria-hidden="true">→</span>
+                Conocer más <span aria-hidden="true">→</span>
               </a>
             </div>
           </div>
@@ -156,6 +164,103 @@ export default function Example() {
           />
         </div>
       </div>
+      <div className="relative overflow-hidden bg-white">
+      <div className="pb-80 pt-16 sm:pb-40 sm:pt-24 lg:pb-48 lg:pt-40">
+        <div className="relative mx-auto max-w-7xl px-4 sm:static sm:px-6 lg:px-8">
+          <div className="sm:max-w-lg">
+            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+            Productos Artesanales
+            </h1>
+            <p className="mt-4 text-xl text-gray-500">
+            Dbit Brands Inc se destaca por sus marcas que crean productos artesanales y de alta calidad, incluyendo ropa, accesorios y zapatos. 
+            </p>
+          </div>
+          <div>
+            <div className="mt-10">
+              {/* Decorative image grid */}
+              <div
+                aria-hidden="true"
+                className="pointer-events-none lg:absolute lg:inset-y-0 lg:mx-auto lg:w-full lg:max-w-7xl"
+              >
+                <div className="absolute transform sm:left-1/2 sm:top-0 sm:translate-x-8 lg:left-1/2 lg:top-1/2 lg:-translate-y-1/2 lg:translate-x-8">
+                  <div className="flex items-center space-x-6 lg:space-x-8">
+                    <div className="grid flex-shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8">
+                      <div className="h-64 w-44 overflow-hidden rounded-lg sm:opacity-0 lg:opacity-100">
+                        <img
+                          src="https://shop.dbit.com.mx/cdn/shop/files/embroidered-simple-backpack-i-bagbase-bg126-anthracite-front-64e17a1e8adc2.jpg?v=1692498475&width=1100"
+                          alt=""
+                          className="h-full w-full object-cover object-center"
+                        />
+                      </div>
+                      <div className="h-64 w-44 overflow-hidden rounded-lg">
+                        <img
+                          src="https://shop.dbit.com.mx/cdn/shop/files/distressed-dad-hat-black-left-side-64e3ee69bd9e1.jpg?v=1692659323&width=1100"
+                          alt=""
+                          className="h-full w-full object-cover object-center"
+                        />
+                      </div>
+                    </div>
+                    <div className="grid flex-shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8">
+                      <div className="h-64 w-44 overflow-hidden rounded-lg">
+                        <img
+                          src="https://shop.dbit.com.mx/cdn/shop/files/mens-high-top-canvas-shoes-white-right-2-64e193d0eb3c5.jpg?v=1692505061&width=1100"
+                          alt=""
+                          className="h-full w-full object-cover object-center"
+                        />
+                      </div>
+                      <div className="h-64 w-44 overflow-hidden rounded-lg">
+                        <img
+                          src="https://shop.dbit.com.mx/cdn/shop/files/womens-athletic-shoes-white-front-64e4023791dfb.jpg?v=1692664386&width=1100"
+                          alt=""
+                          className="h-full w-full object-cover object-center"
+                        />
+                      </div>
+                      <div className="h-64 w-44 overflow-hidden rounded-lg">
+                        <img
+                          src="https://shop.dbit.com.mx/cdn/shop/files/classic-dad-hat-black-front-64e19d99dc299.jpg?v=1692507555&width=713"
+                          alt=""
+                          className="h-full w-full object-cover object-center"
+                        />
+                      </div>
+                    </div>
+                    <div className="grid flex-shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8">
+                      <div className="h-64 w-44 overflow-hidden rounded-lg">
+                        <img
+                          src="https://shop.dbit.com.mx/cdn/shop/files/mens-high-top-canvas-shoes-white-left-64e193d0ec2ff.jpg?v=1692505063&width=1100"
+                          alt=""
+                          className="h-full w-full object-cover object-center"
+                        />
+                      </div>
+                      <div className="h-64 w-44 overflow-hidden rounded-lg">
+                        <img
+                          src="https://shop.dbit.com.mx/cdn/shop/files/snapback-trucker-cap-heather-grey-black-front-64e174a0d541b.jpg?v=1692497098&width=1100"
+                          alt=""
+                          className="h-full w-full object-cover object-center"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <a
+                href="https://shop.dbit.com.mx/"
+                className="inline-block rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-center font-medium text-white hover:bg-indigo-700"
+              >
+                Productos
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
   )
 }
+
+
+
+
+
+
+
